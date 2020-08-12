abacec05060f1ae834f4944820c61d708ea06fb6
Bea DelRosario <bdelronyc@Beas-MacBook-Pro.local>
2020-08-12 16:15:12 -0700

    added components, connected, completed render for CatFacts

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CatFacts from './components/CatFacts'
+
+function App() {
+  return (
+    <div className="App">
+      <CatFacts />
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/CatFacts.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CatFacts = props => {
+    return (
+        <section>
+            <h1>🐱 Random Cat Facts! 🐱</h1>
+        </section>
+    )
+}
+
+export default CatFacts
